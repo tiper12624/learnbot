be953a61c3cc87ef5da63faad29c0b82053eb3a9
tiper12624 <84010051+tiper12624@users.noreply.github.com>
2021-08-24 14:55:54 +0300

    Change environmaent variable name

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_DSN, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
 })
 
