4f99da5c2b257947e487688307d5084dbfd55807
tiper12624 <84010051+tiper12624@users.noreply.github.com>
2021-09-15 16:28:21 +0300

    Cahnge seeder data

--- src/database/seeders/users.js
+++ src/database/seeders/users.js
@@ -2,7 +2,8 @@ module.exports = (db) => {
   db.users.create({
     id: 107797963,
     canAdmin: true,
-    sessionToken: 'g0de6dc2m12h5ilg4r2mb199u55btz89zpvqax48zkhv7dwfpy96pxu6d5r14ome',
+    // learn-session:"HuCH3zAXZrsb%2Bv6ihFCTNdAMEnCs2%2FpvjZHXYYowJbXpf5qtxtZw5E2rbADZ8d9DhJEiLvQpUcXrdUvJSgyuH2FwYv32ENE9jF47TU4Rc1Msht8yNprC8Tsaurh%2BsfvHmy7Iuypt2l0%2BorSRE9CPvEBRhPOBTVvWuqs%3D%3BkOW7K8vyYDix4skWcLj9rN643EOCg09g"
+    sessionToken: 'ymy3682k9kkrm7r1ju0f7d9i47uo34fod8v0t79dia5zh130lm0nyhomo236l0wx',
   }).then()
 
   db.users.create({
