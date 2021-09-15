d2826aebcc3295892b5ff91bfd69972d1d73d291
tiper12624 <84010051+tiper12624@users.noreply.github.com>
2021-09-15 15:57:07 +0300

    Refactor seeders

--- src/database/seeders/results.js
+++ src/database/seeders/results.js
@@ -1,83 +1,83 @@
 module.exports = (db) => {
   db.results.create({
-    userId: 163375600,
+    userId: 113068708,
     questionId: 1,
     answer: 'Ответ №2',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375600,
+    userId: 113068708,
     questionId: 2,
     answer: 'Документ №2',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375600,
+    userId: 113068708,
     questionId: 3,
     answer: 'Изображение №2',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375608,
+    userId: 156815503,
     questionId: 1,
     answer: 'Ответ №1',
     right: 1,
   }).then()
 
   db.results.create({
-    userId: 163375608,
+    userId: 156815503,
     questionId: 2,
     answer: 'Документ №1',
     right: 1,
   }).then()
 
   db.results.create({
-    userId: 163375608,
+    userId: 156815503,
     questionId: 3,
     answer: 'Изображение №1',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375612,
+    userId: 166900829,
     questionId: 1,
     answer: 'Ответ №2',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375612,
+    userId: 166900829,
     questionId: 2,
     answer: 'Документ №2',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375612,
+    userId: 166900829,
     questionId: 3,
     answer: 'Изображение №2',
     right: 0,
   }).then()
 
   db.results.create({
-    userId: 163375616,
+    userId: 242588748,
     questionId: 1,
     answer: 'Ответ №1',
     right: 1,
   }).then()
 
   db.results.create({
-    userId: 163375616,
+    userId: 242588748,
     questionId: 2,
     answer: 'Документ №1',
     right: 1,
   }).then()
 
   db.results.create({
-    userId: 163375616,
+    userId: 242588748,
     questionId: 3,
     answer: 'Изображение №1',
     right: 0,
--- src/database/seeders/users.js
+++ src/database/seeders/users.js
@@ -1,31 +1,31 @@
 module.exports = (db) => {
   db.users.create({
-    id: 163375601,
+    id: 107797963,
     canAdmin: true,
     sessionToken: 'g0de6dc2m12h5ilg4r2mb199u55btz89zpvqax48zkhv7dwfpy96pxu6d5r14ome',
   }).then()
 
   db.users.create({
-    id: 163375600,
+    id: 113068708,
   }).then()
 
   db.users.create({
-    id: 163375608,
+    id: 156815503,
   }).then()
 
   db.users.create({
-    id: 163375612,
+    id: 166900829,
   }).then()
 
   db.users.create({
-    id: 163375616,
+    id: 242588748,
   }).then()
 
   db.users.create({
-    id: 163375620,
+    id: 244795739,
   }).then()
 
   db.users.create({
-    id: 163375624,
+    id: 290370988,
   }).then()
 }
