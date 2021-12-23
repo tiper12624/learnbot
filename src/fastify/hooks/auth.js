c0fc75f9d4fc49d9701782afe8a4e04bcc0b0935
tiper12624 <tiper12624@freeunion.online>
2021-12-23 16:28:21 +0000

    Add route for docker health

--- src/fastify/hooks/auth.js
+++ src/fastify/hooks/auth.js
@@ -2,7 +2,11 @@ const httpErrors = require('http-errors')
 const { db } = require('../../database')
 
 module.exports = (request, reply, next) => {
-  if (request.raw.url.startsWith('/assets') || request.raw.url.startsWith('/login')) {
+  if (
+    request.raw.url.startsWith('/assets')
+    || request.raw.url.startsWith('/login')
+    || request.raw.url.startsWith('/health')
+  ) {
     next()
   } else {
     user = db.users.findOne({
